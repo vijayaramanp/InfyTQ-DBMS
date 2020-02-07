1e6270bb9f4c910f63b37191fee6f2811b93027c
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-02-07 15:31:53 +0530

    Create day2_ex4.sql

---
+++ day2_ex4.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Tournament(
+                        TId integer PRIMARY KEY,
+                        TName Varchar(20)NOT NULL,
+                        StartDt Date not NULL,
+                        EndDt Date NOT NULL,
+                        Prize Integer not null
+                        )
